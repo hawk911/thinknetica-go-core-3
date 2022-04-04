703bd75d361aeed756e56a4bbdd6d7e34aa52575
Denis B <hawk911@yandex.ru>
2022-04-04 23:21:37 +0700

    task 2

---
+++ lesson-2/cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github/hawk911/pkg/crawler"
+	"github/hawk911/pkg/crawler/spider"
+	"github/hawk911/pkg/index"
+	"strings"
+)
+
+var urls = []string{"https://golang.com", "https://go.dev"}
+
+func scan(urls []string, idx index.Service) ([]crawler.Document, error) {
+	var total []crawler.Document
+	var count = 0
+
+	serv := spider.New()
+
+	for _, url := range urls {
+		data, err := serv.Scan(url, 2)
+		if err != nil {
+			fmt.Println("Scan error. Please, try again.")
+			return total, err
+		}
+
+		for _, i := range data {
+			i.ID = count
+			idx.Add(i.Title, i.ID)
+			total = append(total, i)
+			count++
+		}
+	}
+	return total, nil
+}
+func main() {
+
+	idx := index.New()
+	t, err := scan(urls, *idx)
+	if err != nil {
+		fmt.Printf("Error is %s\n", err)
+	}
+
+	sWord := flag.String("s", "", "Please, input word(s)")
+	flag.Parse()
+
+	if *sWord == "" {
+		fmt.Println("Please, read help: -help \n Thanks.")
+		return
+	}
+
+	ids := idx.Search(strings.ToLower(*sWord))
+
+	if len(ids) == 0 {
+		fmt.Println("Nothing was found.")
+		return
+	}
+
+	for _, id := range ids {
+		fmt.Printf("Title: %s\nURL: %s\n\n", t[id].Title, t[id].URL)
+	}
+	fmt.Println("Thanks for using our solution!")
+}
