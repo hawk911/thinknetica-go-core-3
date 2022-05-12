ef12f909e10ebe9e730715081c5137f3c530c6be
Denis B <hawk911@yandex.ru>
2022-05-12 15:51:54 +0700

    refactoring

--- lesson-6/cmd/hw.go
+++ lesson-6/cmd/hw.go
@@ -1,34 +1,11 @@
 package hw
 
 import (
-	"fmt"
 	"math"
 )
 
-// По условиям задачи, координаты не могут быть меньше 0.
+func Distance(x1, y1, x2, y2 float64) float64 {
 
-type Geom struct {
-	X1, Y1, X2, Y2 float64
-}
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 
-func New(X1, Y1, X2, Y2 float64) *Geom {
-	g := Geom{
-		X1: X1,
-		Y1: Y1,
-		X2: X2,
-		Y2: Y2,
-	}
-	return &g
-}
-func (geom *Geom) CalculateDistance() (distance float64) {
-
-	if geom.X1 < 0 || geom.X2 < 0 || geom.Y1 < 0 || geom.Y2 < 0 {
-		fmt.Println("Координаты не могут быть меньше нуля")
-		return -1
-	} else {
-		distance = math.Sqrt(math.Pow(geom.X2-geom.X1, 2) + math.Pow(geom.Y2-geom.Y1, 2))
-	}
-
-	// возврат расстояния между точками
-	return distance
 }
--- lesson-6/cmd/hw_test.go
+++ lesson-6/cmd/hw_test.go
@@ -2,21 +2,27 @@ package hw
 
 import "testing"
 
-func TestGeom_CalculateDistance(t *testing.T) {
+func TestDistance(t *testing.T) {
+	type args struct {
+		x1 float64
+		y1 float64
+		x2 float64
+		y2 float64
+	}
 	tests := []struct {
 		name         string
-		geom         Geom
+		args         args
 		wantDistance float64
 	}{
 		{
 			name:         "#1",
-			geom:         Geom{X1: 1, Y1: 1, X2: 4, Y2: 5},
+			args:         args{x1: 1, y1: 1, x2: 4, y2: 5},
 			wantDistance: 5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotDistance := tt.geom.CalculateDistance(); gotDistance != tt.wantDistance {
+			if gotDistance := Distance(tt.args.x1, tt.args.y1, tt.args.x2, tt.args.y2); gotDistance != tt.wantDistance {
 				t.Errorf("Geom.CalculateDistance() = %v, want %v", gotDistance, tt.wantDistance)
 			}
 		})
